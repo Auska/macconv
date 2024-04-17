1c90fa98cd0dc947e8248338379bedff23bf31d6
Auska <luodan0709@live.cn>
2024-04-17 11:17:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+## MAC地址格式转换
+
+用法：`macconv mac 00:00:00:00:00:00`
+
+```
+Used to convert mac addresses between different devices.
+For example:
+        macconv mac 00:00:00:00:00:00
+
+Usage:
+  macconv [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  help        Help about any command
+  mac         Convert mac address
+  version     Print version.
+
+Flags:
+  -h, --help     help for macconv
+  -t, --toggle   Help message for toggle
+
+Use "macconv [command] --help" for more information about a command.
+```
---
+++ cmd/mac.go
@@ -0,0 +1,74 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// macCmd represents the mac command
+var macCmd = &cobra.Command{
+	Use:   "mac",
+	Short: "Convert mac address",
+	Long:  `Convert mac address`,
+	Run:   getMacAddress,
+}
+
+func init() {
+	rootCmd.AddCommand(macCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// macCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// macCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+func isValidMACAddress(mac string) bool {
+	strLength := len("001122334455")
+	compareStrLength := len(mac)
+
+	if strLength != compareStrLength {
+		return false
+	}
+	// 正则表达式模式用于验证 MAC 地址
+	pattern := `[0-9A-Fa-f]{12}`
+
+	// 编译正则表达式
+	re := regexp.MustCompile(pattern)
+
+	// 使用正则表达式进行匹配
+	return re.MatchString(mac)
+}
+func getMacAddress(cmd *cobra.Command, args []string) {
+	origin := args[0]
+	mac_adress := strings.Replace(strings.Replace(strings.Replace(origin, "-", "", -1), ".", "", -1), ":", "", -1)
+
+	//fmt.Println("mac called", origin)
+	//fmt.Println("mac called", mac_adress)
+
+	if isValidMACAddress(mac_adress) {
+		//fmt.Println("valid mac address")
+		fmt.Println(convertMacAddress(mac_adress, 2, ":"))
+		fmt.Println(convertMacAddress(mac_adress, 4, "."))
+		fmt.Println(convertMacAddress(mac_adress, 4, "-"))
+	} else {
+		fmt.Println("invalid mac address")
+	}
+}
+
+func convertMacAddress(mac_adress string, mac_step_length int, mac_step_str string) string {
+	var mac_address_str string
+	for i := 0; i < len(mac_adress); i += mac_step_length {
+		mac_address_str += mac_adress[i:i+mac_step_length] + mac_step_str
+	}
+	return mac_address_str[:len(mac_address_str)-1]
+}
