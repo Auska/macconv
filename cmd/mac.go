ee0412547523b81a924bd061d75fde8b42efe291
Auska <luodan0709@live.cn>
2025-02-14 09:39:38 +0800

    mac地址格式转换输出大小写两个格式

--- cmd/mac.go
+++ cmd/mac.go
@@ -1,24 +1,3 @@
-/*
-Copyright © 2024 LuoDan<luodan0709@live.cn>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
@@ -39,58 +18,52 @@ var macCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(macCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// macCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// macCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-func isValidMACAddress(mac string) bool {
-	strLength := len("001122334455")
-	compareStrLength := len(mac)
 
-	if strLength != compareStrLength {
+func isValidMACAddress(mac string) bool {
+	if len(mac) != 12 {
 		return false
 	}
-	// 正则表达式模式用于验证 MAC 地址
-	pattern := `[0-9A-Fa-f]{12}`
-
-	// 编译正则表达式
+	pattern := `^[0-9a-f]{12}$`
 	re := regexp.MustCompile(pattern)
-
-	// 使用正则表达式进行匹配
 	return re.MatchString(mac)
 }
+
 func getMacAddress(cmd *cobra.Command, args []string) {
 	if len(args) != 1 {
-		fmt.Printf("missing arguments")
+		fmt.Println("missing arguments")
 		return
 	}
 	origin := args[0]
-	mac_adress := strings.Replace(strings.Replace(strings.Replace(origin, "-", "", -1), ".", "", -1), ":", "", -1)
+	// Remove all separators and convert to lowercase
+	macAddress := strings.ReplaceAll(origin, "-", "")
+	macAddress = strings.ReplaceAll(macAddress, ".", "")
+	macAddress = strings.ReplaceAll(macAddress, ":", "")
+	macAddress = strings.ToLower(macAddress)
 
-	//fmt.Println("mac called", origin)
-	//fmt.Println("mac called", mac_adress)
-
-	if isValidMACAddress(mac_adress) {
-		//fmt.Println("valid mac address")
-		fmt.Println(convertMacAddress(mac_adress, 2, ":"))
-		fmt.Println(convertMacAddress(mac_adress, 4, "."))
-		fmt.Println(convertMacAddress(mac_adress, 4, "-"))
-	} else {
+	if !isValidMACAddress(macAddress) {
 		fmt.Println("invalid mac address")
+		return
 	}
+
+	fmt.Println(macAddress)
+	fmt.Println(convertMacAddress(macAddress, 2, ":"))
+	fmt.Println(convertMacAddress(macAddress, 4, "."))
+	fmt.Println(convertMacAddress(macAddress, 4, "-"))
+	
+	fmt.Println(strings.ToUpper(macAddress))
+	fmt.Println(strings.ToUpper(convertMacAddress(macAddress, 2, ":")))
+	fmt.Println(strings.ToUpper(convertMacAddress(macAddress, 4, ".")))
+	fmt.Println(strings.ToUpper(convertMacAddress(macAddress, 4, "-")))
 }
 
-func convertMacAddress(mac_adress string, mac_step_length int, mac_step_str string) string {
-	var mac_address_str string
-	for i := 0; i < len(mac_adress); i += mac_step_length {
-		mac_address_str += mac_adress[i:i+mac_step_length] + mac_step_str
+func convertMacAddress(mac string, step int, sep string) string {
+	var result strings.Builder
+	for i := 0; i < len(mac); i += step {
+		if i > 0 {
+			result.WriteString(sep)
+		}
+		result.WriteString(mac[i : i+step])
 	}
-	return mac_address_str[:len(mac_address_str)-1]
+	return result.String()
 }
